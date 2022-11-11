ba497b592349dc8734d6198a236579602bb149e7
Rebel5611 <117382036+Rebel5611@users.noreply.github.com>
2022-11-10 22:03:11 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.SwerveDrive;
 import frc.robot.subsystems.Wheel;
+import frc.robot.subsystems.SwerveDrive;
+import frc.robot.subsystems.Autonomous;
+import frc.robot.subsystems.Tracking;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -19,10 +20,10 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final SwerveDrive SwerveDriveSubsystem = new SwerveDrive();
   private final Wheel WheelSubsystem = new Wheel(0, 0);
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(SwerveDriveSubsystem);
+  private final SwerveDrive SwerveDriveSubsystem = new SwerveDrive();
+  private final Autonomous AutonomousSubsystem = new Autonomous();
+  private final Tracking Tracking = new Tracking();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -37,14 +38,4 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {}
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
 }
