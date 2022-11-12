9621a78fcd37b12ceeaf98e684120daff1168406
Rebel5611 <117382036+Rebel5611@users.noreply.github.com>
2022-11-11 18:41:42 -0600

    Add comments

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -82,7 +82,7 @@ public class SwerveDrive extends SubsystemBase {
     // These are later used in the backend, likely to find the angles the wheels need to rotate to when the robot spins 
     Kinematics = new SwerveDriveKinematics(FrontRight.Location, FrontLeft.Location, BackLeft.Location, BackRight.Location);
     
-    // Pass in wheel module locations, as well as robot angle and position for field oriented drive
+    // Pass in wheel module locations, as well as initial robot angle and position for field oriented drive
     // The robot position is unused for now, but might be utilized in autonomous later
     Odometry = new SwerveDriveOdometry(Kinematics, Gyro.getRotation2d(), new Pose2d(0, 0, Gyro.getRotation2d()));
     
@@ -125,16 +125,18 @@ public class SwerveDrive extends SubsystemBase {
     BackRight.setPIDValues(P, I, D);
   }
 
-  // This method does all of the math for the swerve drive, pass in the desired controller inputs
+  // This method does all of the math for the swerve drive, pass in desired translation speeds, spin speed, and the modifier to put on those speeds
+  // Translation and spin speeds on a scale from -1 to 1
+  // Modifiers on a scale from 0 to 1
   // Call during teleopPeriodic()
-  public void swerveDrive(Double RSX, Double RSY, Double RST, Double RSZ, Double LSZ) {
+  public void swerveDrive(Double X, Double Y, Double Spin, Double XYMod, Double SpinMod) {
     // Need to set the gyro angle to a variable in order to invert the output
     GyroRotation2d = Gyro.getRotation2d();
 
-    // Set ChassisSpeeds for actual movement
-    Speeds = ChassisSpeeds.fromFieldRelativeSpeeds((RSY * RSZ), (RSX * RSZ), (RST * LSZ), GyroRotation2d.unaryMinus());
+    // Set the desired speeds for the robot, we also pass in the gyro angle for field oriented drive
+    Speeds = ChassisSpeeds.fromFieldRelativeSpeeds((Y * XYMod), (X * XYMod), (Spin * SpinMod), GyroRotation2d.unaryMinus());
 
-    // Convert to module states
+    // Convert overall robot speeds and angle into speeds and angles for each wheel module, referred to as module states
     SwerveModuleState[] ModuleStates = Kinematics.toSwerveModuleStates(Speeds);
 
     // Front left module state
@@ -149,47 +151,71 @@ public class SwerveDrive extends SubsystemBase {
     // Back right module state
     SwerveModuleState backRight = ModuleStates[3];
 
-    // Update Odometry
+    // Update Odometry, so the robot knows its position on the field
+    // This section currently only exists so we can use odometry, which solves many other issues, however it will likely be useful for autonomous movement.
     Odometry.update(GyroRotation2d.unaryMinus(),
     new SwerveModuleState(FrontLeft.DriveEncoder.getVelocity(), new Rotation2d(FrontLeft.SteerEncoder.getPosition() / EncoderPosMod)),
     new SwerveModuleState(FrontRight.DriveEncoder.getVelocity(), new Rotation2d(FrontRight.SteerEncoder.getPosition() / EncoderPosMod)),
     new SwerveModuleState(BackLeft.DriveEncoder.getVelocity(), new Rotation2d(BackLeft.SteerEncoder.getPosition() / EncoderPosMod)),
     new SwerveModuleState(BackRight.DriveEncoder.getVelocity(), new Rotation2d(BackRight.SteerEncoder.getPosition() / EncoderPosMod)));
     
-    // Optimize rotation positions
+    // Optimize rotation positions, so the wheels don't turn 180 degrees rather than just spinning the drive motor backwards
     var frontLeftOptimized = SwerveModuleState.optimize(frontLeft, new Rotation2d((FrontLeft.SteerEncoder.getPosition() / EncoderPosMod)));
     var frontRightOptimized = SwerveModuleState.optimize(frontRight, new Rotation2d((FrontRight.SteerEncoder.getPosition() / EncoderPosMod)));
     var backLeftOptimized = SwerveModuleState.optimize(backLeft, new Rotation2d((BackLeft.SteerEncoder.getPosition() / EncoderPosMod)));
     var backRightOptimized = SwerveModuleState.optimize(backRight, new Rotation2d((BackRight.SteerEncoder.getPosition() / EncoderPosMod)));
 
-    // Find the distance to the desired wheel angle
+    // Find the differance between the desired wheel angle and the current wheel angle
     FrontRight.DistToPos = ((Math.abs((FrontRight.SteerEncoder.getPosition() / EncoderPosMod) - ((frontRightOptimized.angle.getDegrees() / 360.0)))));
     FrontLeft.DistToPos = ((Math.abs((FrontLeft.SteerEncoder.getPosition() / EncoderPosMod) - ((frontLeftOptimized.angle.getDegrees() / 360.0)))));
     BackLeft.DistToPos = ((Math.abs((BackLeft.SteerEncoder.getPosition() / EncoderPosMod) - ((backLeftOptimized.angle.getDegrees() / 360.0)))));
     BackRight.DistToPos = ((Math.abs((BackRight.SteerEncoder.getPosition() / EncoderPosMod) - ((backRightOptimized.angle.getDegrees() / 360.0)))));
 
-    // More math to set up for the next calculation
-    FrontRight.DistToPos = (1 - (2 * FrontRight.DistToPos));
-    FrontLeft.DistToPos = (1 - (2 * FrontLeft.DistToPos));
-    BackLeft.DistToPos = (1 - (2 * BackLeft.DistToPos));
-    BackRight.DistToPos = (1 - (2 * BackRight.DistToPos));
+    // Math to make the modifier 1 when the current wheel angle is the same as the desired wheel angle, and 0 at the furthest point away.
+    // Original value is multiplied by 4 because, due to angle optimization, the max value the DistToPos variable should be able to reach is .25
+    FrontRight.DistToPos = (1 - (4 * FrontRight.DistToPos));
+    FrontLeft.DistToPos = (1 - (4 * FrontLeft.DistToPos));
+    BackLeft.DistToPos = (1 - (4 * BackLeft.DistToPos));
+    BackRight.DistToPos = (1 - (4 * BackRight.DistToPos));
+    
+    // Make absolutely sure the DistToPos variable is greater than or equal to 0, jsut in case the code does something dumb
+    if (FrontRight.DistToPos < 0) {
+      FrontRight.DistToPos = 0;
+    }
+    if (FrontLeft.DistToPos < 0) {
+      FrontLeft.DistToPos = 0;
+    }
+    if (BackLeft.DistToPos < 0) {
+      BackLeft.DistToPos = 0;
+    }
+    if (BackRight.DistToPos < 0) {
+      BackRight.DistToPos = 0;
+    }
 
-    // Create modifier to decrease the drive speed of the wheel exponentially the farther it is from the desired angle
+    // Use the DistToPos variable to create a DistSpdMod variable
+    // Used to ramp speed up to the desired speed exponentially as the wheel gets closer to the desired angle
     FrontRight.DistSpdMod = Math.pow(FrontRight.DistToPos, 5);
     FrontLeft.DistSpdMod = Math.pow(FrontLeft.DistToPos, 5);
     BackLeft.DistSpdMod = Math.pow(BackLeft.DistToPos, 5);
     BackRight.DistSpdMod = Math.pow(BackRight.DistToPos, 5);
     
+    // Set the PrevRampedWheelSpd variable to the speed the motors were set to the last time the code was run
     FrontRight.PrevRampedWheelSpd = FrontRight.RampedWheelSpd;
     FrontLeft.PrevRampedWheelSpd = FrontLeft.RampedWheelSpd;
     BackLeft.PrevRampedWheelSpd = BackLeft.RampedWheelSpd;
     BackRight.PrevRampedWheelSpd = BackRight.RampedWheelSpd;
-      
+    
+    // Set the RampedWheelSpd variable to the desired wheel speed
+    // This is done so the speed is still set even if the following if statements return false
     FrontRight.RampedWheelSpd = ((frontRightOptimized.speedMetersPerSecond / 2) * FrontRight.DistSpdMod);
     FrontLeft.RampedWheelSpd = ((frontLeftOptimized.speedMetersPerSecond / 2) * FrontLeft.DistSpdMod);
     BackLeft.RampedWheelSpd = ((backLeftOptimized.speedMetersPerSecond / 2) * BackLeft.DistSpdMod);
     BackRight.RampedWheelSpd = ((backRightOptimized.speedMetersPerSecond / 2) * BackRight.DistSpdMod);
     
+    // Determine if the difference in current speed and desired speed is greater than the maximum desired difference (the DriveRampValue variable)
+    // If the difference is greater than the maximum desired difference, then find out if the change is greater than or less than zero
+    // If the change is greater than zero, then add the maximum desired difference to the previous speed and set that to the new desired speed
+    // If the change is less than zero, then subtract the maximum desired difference from the previous speed and set that to the new desired speed
     if (Math.abs(FrontRight.RampedWheelSpd - FrontRight.PrevRampedWheelSpd) > DriveRampValue) {
       if ((FrontRight.RampedWheelSpd - FrontRight.PrevRampedWheelSpd) > 0) {
         FrontRight.RampedWheelSpd = (FrontRight.PrevRampedWheelSpd + DriveRampValue);
@@ -223,16 +249,19 @@ public class SwerveDrive extends SubsystemBase {
       }
     }
 
-
-    // Actually tell the wheel to turn to the correct position
-    if ((Math.abs(RSX) + Math.abs(RSY) + Math.abs(RST)) != 0) {
+    // Tell the steer motors to turn the wheels to the correct position
+    // An issue is created by ramping which this if statement solves, I will explain the roots of the problem, and the solution here:
+    // If all inputs for robot speeds are 0, the angle for the wheels will default to 0
+    // This causes a problem because the drive wheel speed does not instantly go to zero, causing the robot's direction to change
+    // This if statement fixes this issue by only changing the angle of the wheels if any of the desired robot speeds are greater than 0
+    if ((Math.abs(X) + Math.abs(Y) + Math.abs(Spin)) != 0) {
       FrontRight.SteerPIDController.setReference(((frontRightOptimized.angle.getDegrees() / 360.0) * EncoderPosMod), ControlType.kPosition);
       FrontLeft.SteerPIDController.setReference(((frontLeftOptimized.angle.getDegrees() / 360.0) * EncoderPosMod), ControlType.kPosition);
       BackLeft.SteerPIDController.setReference(((backLeftOptimized.angle.getDegrees() / 360.0) * EncoderPosMod), ControlType.kPosition);
       BackRight.SteerPIDController.setReference(((backRightOptimized.angle.getDegrees() / 360.0) * EncoderPosMod), ControlType.kPosition);
     }
     
-    // Actually tell the wheel to turn at the correct speed
+    // Tell the drive motors to drive the wheels at the correct speed
     FrontRight.Drive.set(FrontRight.RampedWheelSpd);
     FrontLeft.Drive.set(FrontLeft.RampedWheelSpd);
     BackLeft.Drive.set(BackLeft.RampedWheelSpd);
