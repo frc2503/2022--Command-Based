dbdedb31d4eb1619a4f2a7b14cf2aaaeb18a2b38
Rebel5611 <117382036+Rebel5611@users.noreply.github.com>
2022-11-15 20:35:38 -0600

    Switch to Talon SRX for Steer motors
    Switch to Talon SRX for Steer motor controllers, add better descriptions on methods, and remove unused imports

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,182 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+/**
+Class to hold all code for the swerve drive
+Mostly exists to prevent people from accidentally messing the code up
+*/
+public class SwerveDrive extends SubsystemBase {
+  // Define all objects and varibles used by this class
+  private AHRS Gyro;
+  private Rotation2d GyroRotation2d;
+
+  private SwerveDriveKinematics Kinematics;
+  private SwerveDriveOdometry Odometry;
+
+  private double EncoderPosMod;
+  private double DriveRampValue;
+
+  private ChassisSpeeds Speeds;
+  private SwerveModuleState[] ModuleStates;
+
+  public Wheel FrontRight;
+  public Wheel FrontLeft;
+  public Wheel BackLeft;
+  public Wheel BackRight;
+
+  /**
+   * SwerveDrive class constructor, initializes all variables, objects, and methods for the created SwerveDrive object
+   */
+  public SwerveDrive() {
+    // Create objects for the Wheel class, and define locations of wheel modules compared to robot center, which doesn't really matter unless base is MUCH longer on one side
+    FrontRight = new Wheel(0.355, -0.381);
+    FrontLeft = new Wheel(0.355, 0.381);
+    BackLeft = new Wheel(-0.355, -0.381);
+    BackRight = new Wheel(-0.355, 0.381);
+
+    // Create a ChassisSpeeds object, which we later pass our desired speeds into to get our wheel speeds and angles
+    Speeds = new ChassisSpeeds();
+    
+    // Initialize and zero gyro
+    Gyro = new AHRS(I2C.Port.kMXP);
+    Gyro.calibrate();
+    Gyro.reset();
+    
+    // Pass in locations of wheels relative to the center of the robot
+    // These are later used in the backend, likely to find the angles the wheels need to rotate to when the robot spins 
+    Kinematics = new SwerveDriveKinematics(FrontRight.Location, FrontLeft.Location, BackLeft.Location, BackRight.Location);
+    
+    // Pass in wheel module locations, as well as initial robot angle and position for field oriented drive
+    // The robot position is unused for now, but might be utilized in autonomous later
+    Odometry = new SwerveDriveOdometry(Kinematics, Gyro.getRotation2d(), new Pose2d(0, 0, new Rotation2d()));
+    
+    // Number to modify the encoders' output values.
+    // Used to get all angle values to the same scale for calculations
+    EncoderPosMod = 1023;
+
+    // Amount the drive speed can increase or decrease by, max value of 1, min value of 0
+    // Purposefully set very low because of how quickly the code runs
+    DriveRampValue = .02;
+  }
+
+  /**
+	 * Assign motor controllers their CAN numbers, and call the initEncodersAndPIDControllers() method for each wheel module
+	 *
+	 * @param FRD
+	 *            CAN number of the front right drive motor controller
+	 * @param FRS
+	 *            CAN number of the front right steer motor controller
+   * @param FLD
+	 *            CAN number of the front left drive motor controller
+   * @param FLS
+	 *            CAN number of the front left steer motor controller
+   * @param BLD
+	 *            CAN number of the back left drive motor controller
+   * @param BLS
+	 *            CAN number of the back left steer motor controller
+   * @param BRD
+	 *            CAN number of the back right drive motor controller
+   * @param BRS
+	 *            CAN number of the back right steer motor controller
+	 */
+  public void initMotorControllers(Integer FRD, Integer FRS, Integer FLD, Integer FLS, Integer BLD, Integer BLS, Integer BRD, Integer BRS) {
+    FrontRight.Drive = new CANSparkMax(FRD, MotorType.kBrushless);
+    FrontRight.Steer = new TalonSRX(FRS);
+    FrontLeft.Drive = new CANSparkMax(FLD, MotorType.kBrushless);
+    FrontLeft.Steer = new TalonSRX(FLS);
+    BackRight.Drive = new CANSparkMax(BRD, MotorType.kBrushless);
+    BackRight.Steer = new TalonSRX(BRS);
+    BackLeft.Drive = new CANSparkMax(BLD, MotorType.kBrushless);
+    BackLeft.Steer = new TalonSRX(BLS);
+    
+    FrontRight.initEncodersAndPIDControllers();
+    FrontLeft.initEncodersAndPIDControllers();
+    BackLeft.initEncodersAndPIDControllers();
+    BackRight.initEncodersAndPIDControllers();
+  }
+
+  /**
+   * Call the setPIDValues() method for each wheel module
+   * 
+   * @param P
+	 *            Proportional value
+   * @param I
+	 *            Integral value
+   * @param D
+	 *            Derivative value
+   */
+  public void setPID(Double P, Double I, Double D) {
+    FrontRight.setPIDValues(P, I, D);
+    FrontLeft.setPIDValues(P, I, D);
+    BackLeft.setPIDValues(P, I, D);
+    BackRight.setPIDValues(P, I, D);
+  }
+
+  /**
+   * This method does all of the math for the swerve drive, and outputs to the wheel modules
+   * 
+   * @param X
+	 *            Desired X speed of the robot from -1 to 1
+   * @param Y
+	 *            Desired Y speed of the robot from -1 to 1
+   * @param Spin
+	 *            Desired rotational speed of the robot from -1 to 1
+   * @param XYMod
+   *            Number to multiply the translation speed of the robot by, to modify speed while driving
+   * @param SpinMod
+   *            Number to multiply the rotation speed of the robot by, to modify speed while driving
+   */
+  public void swerveDrive(Double X, Double Y, Double Spin, Double XYMod, Double SpinMod) {
+    // Need to set the gyro angle to a variable in order to invert the output
+    GyroRotation2d = Gyro.getRotation2d();
+
+    // Set the desired speeds for the robot, we also pass in the gyro angle for field oriented drive
+    Speeds = ChassisSpeeds.fromFieldRelativeSpeeds((Y * XYMod), (X * XYMod), (Spin * SpinMod), GyroRotation2d.unaryMinus());
+
+    // Convert overall robot speeds and angle into speeds and angles for each wheel module, referred to as module states
+    ModuleStates = Kinematics.toSwerveModuleStates(Speeds);
+
+    // Front left module state
+    FrontLeft.ModuleState = ModuleStates[0];
+
+    // Front right module state
+    FrontRight.ModuleState = ModuleStates[1];
+
+    // Back left module state
+    BackLeft.ModuleState = ModuleStates[2];
+
+    // Back right module state
+    BackRight.ModuleState = ModuleStates[3];
+
+    // Update Odometry, so the robot knows its position on the field
+    // This section currently only exists so we can use odometry, which solves many other issues, however it will likely be useful for autonomous movement.
+    Odometry.update(GyroRotation2d.unaryMinus(),
+    new SwerveModuleState(FrontLeft.DriveEncoder.getVelocity(), new Rotation2d(FrontLeft.Steer.getSelectedSensorPosition() / EncoderPosMod)),
+    new SwerveModuleState(FrontRight.DriveEncoder.getVelocity(), new Rotation2d(FrontRight.Steer.getSelectedSensorPosition() / EncoderPosMod)),
+    new SwerveModuleState(BackLeft.DriveEncoder.getVelocity(), new Rotation2d(BackLeft.Steer.getSelectedSensorPosition() / EncoderPosMod)),
+    new SwerveModuleState(BackRight.DriveEncoder.getVelocity(), new Rotation2d(BackRight.Steer.getSelectedSensorPosition() / EncoderPosMod)));
+    
+    // Do math for swerve drive that is identical between all wheel modules, and then send the angle and speed to the wheels
+    FrontRight.swerveDriveSetOutputs(X, Y, Spin, EncoderPosMod, DriveRampValue);
+    FrontLeft.swerveDriveSetOutputs(X, Y, Spin, EncoderPosMod, DriveRampValue);
+    BackLeft.swerveDriveSetOutputs(X, Y, Spin, EncoderPosMod, DriveRampValue);
+    BackRight.swerveDriveSetOutputs(X, Y, Spin, EncoderPosMod, DriveRampValue);
+  }
+}
