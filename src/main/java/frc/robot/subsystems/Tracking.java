f3c4a87b3f6f3c2e7c7902a37611b5f6e9959d7b
warrior-robotics <warriorrobotics@outlook.com>
2022-12-01 16:26:41 -0600

    Start work of Limelight Tracking

--- src/main/java/frc/robot/subsystems/Tracking.java
+++ src/main/java/frc/robot/subsystems/Tracking.java
@@ -4,14 +4,32 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Tracking extends SubsystemBase {
-  /** Creates a new Tracking. */
-  public Tracking() {}
+  public NetworkTableInstance Inst;
+  public NetworkTable Table;
+  public NetworkTableEntry HasTarget;
+  public NetworkTableEntry TargetOffsetH;
+  public NetworkTableEntry TargetOffsetV;
+  public NetworkTableEntry TargetArea;
+  public NetworkTableEntry TargetSkew;
+
+  public Tracking() {
+    Inst = NetworkTableInstance.getDefault();
+    Table = Inst.getTable("limelight");
+    HasTarget = Table.getEntry("tv");
+    TargetOffsetH = Table.getEntry("tx");
+    TargetOffsetV = Table.getEntry("ty");
+    TargetArea = Table.getEntry("ta");
+    TargetSkew = Table.getEntry("ts");
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    
   }
 }
